da3ad053afc30c6f4194eb786eed451872335f5a
Your Name <your_email@example.com>
2025-03-06 18:58:45 +0300

    changes

--- jobsite/settings.py
+++ jobsite/settings.py
@@ -73,11 +73,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
